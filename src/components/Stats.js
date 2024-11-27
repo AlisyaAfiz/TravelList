fdef9fe5dcaa46c0d699a50dc75861aa1145719f
AlisyaAfiz <23018913@myrp.edu.sg>
2024-11-27 14:21:28 +0800

    Updated Folders

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,7 +1,7 @@
 export default function Stats({items}) {
     const totalItems = items.length;
     const packedItems = items.filter((item) => item.packed).length;
-    const percentage = Math.round((packedItems/totalItems)*100)
+    const percentage = totalItems === 0 ? 0 : Math.round((packedItems/totalItems)*100)
     return (
       <footer className="stats">
         <em style={{fontSize: 27}}>
